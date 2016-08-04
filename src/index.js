3c8d3c05f6d16aafc1860e6f415825549fbb9f55
alally <andra.joy.lally@zalando.de>
2016-08-04 09:17:42 +0200

    add swap components:

--- src/index.js
+++ src/index.js
@@ -4,20 +4,93 @@
 
 import React from 'react';
 
-class MyComponent extends React.Component {
-    componentDidMount() {
-        // some logic here - we only test if the method is called
+// class MyComponent extends React.Component {
+//     componentDidMount() {
+//         // some logic here - we only test if the method is called
+//     }
+//     render() {
+//         return (
+//             <div className="my-component">
+//                 <i className="icon-test"></i>
+//                 <i className="icon-test"></i>
+//                 <i className="icon-test"></i>
+//                 <button onClick={this.props.handleClick} type="button">here is your package</button>
+//             </div>
+//         )
+//     }
+// };
+
+import React, { Component, PropTypes } from 'react';
+
+export class ComponentSwapper extends Component {
+
+    static Proptypes = {
+        max: PropTypes.number
     }
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            windowWidth: 400
+        };
+    }
+
+    componentWillMount() {
+        window.addEventListener('resize', () => this.handleResize());
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('resize', () => this.handleResize());
+    }
+
+    handleResize() {
+        this.setState({ windowWidth: window.innerWidth });
+    }
+    _renderMaxChild() {
+        const { children } = this.props;
+
+        if (Array.isArray(children)) {
+            return children.reduce((res, c) => {
+                let defaultKey = Object.keys(c).includes('default');
+                let max = c.props.max;
+                if (max <= this.state.windowWidth) {
+                    if (res.props.max < max) {
+                        return c;
+                    } else {
+                        return res;
+                    }
+                } else {
+                    return res;
+                }
+                if (defaultKey && res === null) {
+                    return c;
+                }
+
+            }, {props: {max: null}});
+        } else {
+            return children;
+        }
+    }
+
     render() {
         return (
-            <div className="my-component">
-                <i className="icon-test"></i>
-                <i className="icon-test"></i>
-                <i className="icon-test"></i>
-                <button onClick={this.props.handleClick} type="button">here is your package</button>
+            <div>
+                {this._renderMaxChild()}
             </div>
-        )
+        );
     }
-};
+}
 
-export default MyComponent;
+
+import React, { Component } from 'react';
+
+export class Swap extends Component {
+
+    render() {
+        return (
+            <div>
+                {this.props.children}
+            </div>
+        );
+    }
+}
